fbc0aec3035577b2da60254e1169a2f107dd5884
umesh-nethmina <umeshnethmina118@gmail.com>
2026-06-12 15:29:31 +0530

    feat: add stb_image library and implement asset-based character sprite rendering

--- Dino.h
+++ Dino.h
@@ -1,19 +1,50 @@
 #ifndef DINO_H
 #define DINO_H
 
+#include <GL/glut.h>
+
+enum DinoState {
+    RUNNING,
+    JUMPING,
+    DUCKING,
+    DEAD
+};
+
+struct SpriteFrame {
+    float width;
+    float height;
+    float snoutX;
+};
+
 struct Dino {
     float x;
     float y;
     float width;
     float height;
     float velocity;
-    bool isJumping;
+    DinoState state;
+    int currentFrame;
+    int frameCounter;
+    bool isMovingLeft;
+    bool isMovingRight;
+    bool hasDoubleJumped;
+    bool facingLeft;
+
+    GLuint runTextures[3];
+    GLuint duckTextures[3];
+    SpriteFrame runFrames[3];
+    SpriteFrame duckFrames[3];
 
     Dino();
     void init();
     void update();
     void draw();
     void jump();
+    void duck();
+    void unduck();
+    void die();
+    
+    void loadTextures();
 };
 
 #endif
